238a73ff16a1bd0d65afcfbc436e926016cb7520
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2021-04-26 01:09:54 +0600

    Moved to Skill-track

---
+++ Skill Track - Machine Learning Scientist with Python/Unsupervised Learning in Python/Chapter 4 - Discovering interpretable features/1.3 NMF reconstructs samples.py
@@ -0,0 +1,14 @@
+In this exercise, you'll check your understanding of how NMF reconstructs samples from its components using the NMF feature values. On the right are the components of an NMF model. If the NMF feature values of a sample are [2, 1], then which of the following is most likely to represent the original sample? A pen and paper will help here! You have to apply the same technique Ben used in the video to reconstruct the sample [0.1203 0.1764 0.3195 0.141].
+
+Instructions
+50 XP
+Possible Answers
+
+[2.2, 1.1, 2.1].
+
+[0.5, 1.6, 3.1].
+
+[-4.0, 1.0, -2.0].
+
+
+Ans: [2.2, 1.1, 2.1].
