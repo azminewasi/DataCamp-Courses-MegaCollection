6be4f1be8ce12dfaf28c8a4166928fc413d9abf6
Azmine Toushik Wasi <azmine.wasi@gmail.com>
2021-04-27 23:49:32 +0600

    Optimizing a neural network with backward propagation

--- Introduction to Deep Learning in Python/Chapter 2 - Optimizing a neural network with backward propagation/2.2 Improving model weights.py
+++ Introduction to Deep Learning in Python/Chapter 2 - Optimizing a neural network with backward propagation/2.2 Improving model weights.py
@@ -12,3 +12,29 @@ Calculate the error for the new predictions. Store the result as error_updated.
 Hit 'Submit Answer' to compare the updated error to the original!
 
 '''
+# Set the learning rate: learning_rate
+learning_rate = 0.01
+
+# Calculate the predictions: preds
+preds = (weights * input_data).sum()
+
+# Calculate the error: error
+error = preds - target
+
+# Calculate the slope: slope
+slope = 2 * input_data * error
+
+# Update the weights: weights_updated
+weights_updated = weights-slope*learning_rate
+
+# Get updated predictions: preds_updated
+preds_updated = (weights_updated *input_data).sum()
+
+# Calculate updated error: error_updated
+error_updated = preds_updated - target
+
+# Print the original error
+print(error)
+
+# Print the updated error
+print(error_updated)
